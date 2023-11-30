6d337a8152daccb2b7c6e0896781cab4dedb8430
rivaannn <mrivans2002@gmail.com>
2023-12-01 00:17:15 +0700

    Memperbaiki navbar dan mengubah footer

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -25,7 +25,7 @@
                 class="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                     <a href="/"
-                        class="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $active === 'home' ? 'text-blue-700' : 'text-black' }}"
+                        class="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ $active === 'home' ? 'text-blue-700' : '' }}"
                         aria-current="page">Home</a>
                 </li>
                 <li>

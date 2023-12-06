5b49ac0bbb425f445623c70fdab150d04a4c96e4
rejka permana <permanarejka@gmail.com>
2023-12-06 16:20:41 +0700

    writter table created

---
+++ app/Http/Controllers/WritterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreWritterRequest;
+use App\Http\Requests\UpdateWritterRequest;
+use App\Models\Writter;
+
+class WritterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWritterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Writter $writter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Writter $writter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWritterRequest $request, Writter $writter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Writter $writter)
+    {
+        //
+    }
+}

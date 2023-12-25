0cc9d619f768bb877dd16221ff3e2cdf58b7779b
Anggakusuma74 <kusumaangga719@gmail.com>
2023-12-26 00:50:34 +0700

    Membuat darkmode di dashboard user

--- resources/views/dashboarduser/keranjang.blade.php
+++ resources/views/dashboarduser/keranjang.blade.php
@@ -4,11 +4,8 @@
             {{ __('Daftar Keranjang') }}
         </h2>
     </x-slot>
-<<<<<<< HEAD
     
     
 
-=======
->>>>>>> 2066cb6aa45a79c217e488dfafa59a87efab6532
     @include('partials.footer')
 </x-appuser>
